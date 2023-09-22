68a5e55b1369e0d85bfa19fbc04ef142f2228353
zmalkmus <zmalkmus@vols.utk.edu>
2023-09-22 14:26:43 -0400

    hash attack

--- HashAttack/graph.py
+++ HashAttack/graph.py
@@ -2,19 +2,22 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+palette = sns.color_palette("crest")
+
 # ==========================================================
 # Collisions
 # ==========================================================
 
 # Load the data from the CSV file into a Pandas DataFrame
-data = pd.read_csv('collision.csv')
+df = pd.read_csv('collision.csv')
 
 # Create a box plot using Seaborn
 plt.figure(figsize=(10, 6))  # Optional: Set the figure size
 sns.set(style="whitegrid")   # Optional: Set the style
 
 # Create the box plot
-ax1 = sns.boxplot(x="n_bits", y="n_tries", data=data)
+ax1 = sns.boxplot(x="n_bits", y="n_tries", data=df, showmeans=True, palette=palette,
+                  meanprops={"markerfacecolor": "black", "markeredgecolor": "black"})
 
 # Optional: Add labels and a title
 plt.xlabel("Number of Bits")
@@ -25,21 +28,22 @@ plt.title("Collision Attack")
 ax1.set_yscale("log")
 
 # Show the plot
-plt.show()
+plt.savefig('images/collision.png')
 
 # ==========================================================
 # Preimage
 # ==========================================================
 
 # Load the data from the CSV file into a Pandas DataFrame
-data = pd.read_csv('preimage.csv')
+df = pd.read_csv('preimage.csv')
 
 # Create a box plot using Seaborn
 plt.figure(figsize=(10, 6))  # Optional: Set the figure size
 sns.set(style="whitegrid")   # Optional: Set the style
 
 # Create the box plot
-ax2 = sns.boxplot(x="n_bits", y="n_tries", data=data)
+ax2 = sns.boxplot(x="n_bits", y="n_tries", data=df, showmeans=True, palette=palette,
+                  meanprops={"markerfacecolor": "red", "markeredgecolor": "red"})
 
 # Optional: Add labels and a title
 plt.xlabel("Number of Bits")
@@ -50,4 +54,4 @@ plt.title("Preimage Attack")
 ax2.set_yscale("log")
 
 # Show the plot
-plt.show()
+plt.savefig('images/preimage.png')
